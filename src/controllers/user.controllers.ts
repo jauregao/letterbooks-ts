9597433434ce99372e738b2e0780f8089832cdc0
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-02-03 15:12:38 -0300

    feat: cria a pasta controladores e as funções das rotas de usuarios

---
+++ src/controllers/user.controllers.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express'
+
+export const getUserLogged = async (req: Request, res: Response) => {
+  return 1
+}
+
+export const newUser = async (req: Request, res: Response) => {
+  return 1
+}
+
+export const login = async (req: Request, res: Response) => {
+  return 1
+}
+
+export const updateUser = async (req: Request, res: Response) => {
+  return 1
+}
+
+export const deleteUser = async (req: Request, res: Response) => {
+  return 1
+}
