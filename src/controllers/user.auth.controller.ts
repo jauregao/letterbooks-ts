79cdded6176ca721adcd27152812ed679fab1591
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-02-07 21:27:24 -0300

    feat: implementa a autenticação de usuario com jsonwebtoken

---
+++ src/controllers/user.auth.controller.ts
@@ -0,0 +1,41 @@
+import { knex } from '../connections/connections';
+import jwt, { Secret } from 'jsonwebtoken'
+import { NextFunction, Request, Response } from 'express'
+
+
+interface CustomRequest extends Request {
+  usuario?: Record<string, any>
+}
+
+const secretKey: Secret = process.env.JWT_SECRET_KEY ?? 'abcdef'
+
+export const verifyUserIsLogged = async (req: CustomRequest, res: Response, next: NextFunction) => {
+
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json("Não autorizado");
+  }
+
+  try {
+    const token: string = authorization.replace("Bearer ", "").trim();
+
+    const { id } = jwt.verify(token, secretKey) as { id: string };
+
+    const query = await knex("usuarios").where({ id }).first();
+
+    if (!query) {
+      return res.status(404).json("Usuario não encontrado");
+    }
+
+    const { senha, ...usuario } = query;
+
+    req.usuario = usuario;
+
+    next();
+  } catch (error) {
+    return res.status(500).json({ mensagem: "Erro interno do servidor." });
+  }
+};
+
+
