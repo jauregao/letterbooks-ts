6171de02c64493507ccb0835aec175d026da0931
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-02-26 17:32:57 -0300

    fix: corrige o problema de chave do jwt e retorno da função

--- src/middlewares/userAuth.ts
+++ src/middlewares/userAuth.ts
@@ -2,30 +2,29 @@ import { knex } from '../connections/connections'
 import jwt, { Secret } from 'jsonwebtoken'
 import { NextFunction, Request, Response } from 'express'
 
-
 interface CustomRequest extends Request {
   usuario?: Record<string, any>
 }
 
-const secretKey: Secret = process.env.JWT_SECRET_KEY ?? 'abcdef'
+const secretKey: Secret = process.env.JWT_SECRET_KEY!
 
-export const verifyUserIsLogged = async (req: CustomRequest, res: Response, next: NextFunction) => {
+export const verifyUserIsLogged = async (req: CustomRequest, res: Response, next: NextFunction): Promise<void | Object> => {
 
   const { authorization } = req.headers
 
   if (!authorization) {
-    return res.status(401).json("Não autorizado")
+    return res.status(401).json({message : 'Não autorizado'})
   }
 
   try {
-    const token: string = authorization.replace("Bearer ", "").trim()
+    const token: string = authorization.replace('Bearer ', '').trim()
 
     const { id } = jwt.verify(token, secretKey) as { id: string }
 
-    const query = await knex("usuarios").where({ id }).first()
+    const query = await knex('usuarios').where({ id }).first()
 
     if (!query) {
-      return res.status(404).json("Usuario não encontrado")
+      return res.status(404).json({message : 'Usuario não encontrado'})
     }
 
     const { senha, ...usuario } = query
@@ -34,6 +33,6 @@ export const verifyUserIsLogged = async (req: CustomRequest, res: Response, next
 
     next()
   } catch (error) {
-    return res.status(500).json({ mensagem: "Erro interno do servidor." })
+    return res.status(500).json({ mensagem: 'Erro interno do servidor.' })
   }
 }
